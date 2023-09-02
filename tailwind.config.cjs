a3f3663dbdeba06a5d1bc4e3dd6c9ad240832ef0
maros Studenic <studenic.maros@gmail.com>
2023-09-02 20:11:19 +0100

    add faq

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT({
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"node_modules/@material-tailwind/html/components/**/*.{js,ts,jsx,tsx}",
+		"node_modules/@material-tailwind/html/theme/components/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
-}
+});
