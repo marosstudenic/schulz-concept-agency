2a63ae085c95c6868d25a3931ab1d07aa520e81c
Maros Studenic <studenic.maros@gmail.com>
2023-12-16 16:13:17 +0000

    add basics of multilang site

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'English',
+  sk: 'Slovensky',
+};
+
+export const defaultLang = 'sk';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+  sk: {
+    'nav.home': 'Domov',
+    'nav.about': 'O nás',
+    'nav.twitter': 'Twitter',
+  },
+} as const;
